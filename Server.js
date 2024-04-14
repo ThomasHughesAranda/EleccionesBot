4bbd8065dd33880f619dbc9d4a74f669a99aeb53
ThomasHughesAranda <th.tomy2000@gmail.com>
2024-04-13 23:36:23 -0400

    Se agrega .env y OpenAI a las dependencias del proyecto

--- Server.js
+++ Server.js
@@ -1,9 +1,25 @@
-const PORT=8080
-const express = require('express')
-const cors = require('cors')
-const app = express()
-app.use(express.json())
-app.use(cors())
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { OpenAI } = require('openai');
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(PORT,() =>console.log('Server is running on port '+PORT))
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+openai.beta.assistants.retrieve("asst_A0uu0dkrl9sIXInopXdRYQNk")
+    .then(assistant => {
+        console.log(assistant);
+    })
+    .catch(error => {
+        console.error("Error retrieving assistant:", error);
+    });
+
+app.listen(PORT, () => console.log('El servidor está corriendo en el puerto ' + PORT));
